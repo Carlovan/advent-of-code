938644ca976998954cf3f0cb9180104c87f5162b
Carlovan <giulio.carlassare@gmail.com>
2023-12-10 20:23:15 +0100

    feat(2023): add day 9 solutions

---
+++ 2023/day09b/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::iter::successors;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()>{
+    // Open the file
+    let file_path = "input.txt";
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let output: i64 = reader.lines().map(|line| {
+        let line = line.unwrap();
+
+        let seq: Vec<i64> = line.split(' ').map(|v| v.parse().unwrap()).collect();
+        
+        let adding: i64 = successors(Some(seq.clone()), |s: &Vec<i64>| -> Option<Vec<i64>> {
+            // println!("{:?}", s.iter().zip(s.iter().skip(1)).collect::<Vec<_>>());
+            Some(s.iter().zip(s.iter().skip(1)).map(|(a, b)| b - a).collect())
+        })
+        .skip(1)
+        // .inspect(|s| println!("B take_while {:?}", s))
+        .take_while(|s| !(s.iter().min() == Some(&0) && s.iter().max() == Some(&0)))
+        // .inspect(|s| println!("A take_while {:?}", s))
+        .map(|s| *s.get(0).unwrap())
+        .zip([-1, 1].iter().cycle())
+        .map(|(v, m)| v*m)
+        // .inspect(|x| println!("{}", x))
+        .sum();
+
+        seq.get(0).unwrap() + adding
+    })
+    // .inspect(|x| println!("{}", x))
+    .sum();
+    println!("{}", output);
+
+    Ok(())
+}
